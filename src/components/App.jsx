d8a8001220108c157d7c6a8dbdfe9b7ed2661e79
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-02-04 12:23:28 +0200

    commit_main-upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,94 @@
-import { Component } from "react";
-import { nanoid } from "nanoid";
-import { ContactForm } from "./ContactForm/ContactForm.jsx"
-import { ContactList } from "./ContactList/ContactList.jsx";
-import { Filter } from "./Filter/Filter.jsx";
-import { contacts } from "./contacts.js";
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm.jsx';
+import { ContactList } from './ContactList/ContactList.jsx';
+import { Filter } from './Filter/Filter.jsx';
 
 export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
 
-   state = {
-       contacts: [...contacts], // default array from contacts.js
-       filter: ''
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('my-contacts'));
+    if (contacts && contacts.length) {
+      // cheking for if 'my-contacts' is in LS and it lenght > 0, than 'true'
+      this.setState({ contacts });
     }
+  }
 
-addContact = ({name, number}) => {
-            if (this.isDublicate(name, number)) {    // cheking for dublicate in state list
-                return alert(`${name} is already in contacts`)
-      }
-      this.setState(prevState => {
-        const { contacts } = prevState;
-        
-            const newContact = {
-                id: nanoid(),
-                name,
-                number, 
-            }
-            return {contacts: [newContact, ...contacts], name:"", number:""}
-        })       
-}
+  componentDidUpdate(prevProps, prevState) {
+    // console.log("prevState", prevState);
+    // console.log('currentState', this.state);
+    const { contacts } = this.state;
+    if (prevState.contacts.length !== contacts.length) {
+      // setItem to LS works only if add new contact, but not for filter changes
+      localStorage.setItem('my-contacts', JSON.stringify(contacts));
+    }
+  }
 
-deleteContact = (id) => {
-        this.setState(({ contacts }) => {
-            const newContacts = contacts.filter(contact => contact.id !== id);
-            return { contacts: newContacts}
-       })
-}
+  addContact = ({ name, number }) => {
+    if (this.isDublicate(name, number)) {
+      // cheking for dublicate in state list
+      return alert(`${name} is already in contacts`);
+    }
+    this.setState(prevState => {
+      const { contacts } = prevState;
 
-isDublicate(name, number) {
-        const normalizedName = name.toLowerCase();
-        const normalizedNumber = number.toLowerCase();
-        const { contacts } = this.state;
-        const result = contacts.find(({ name, number }) => {
-            return (name.toLowerCase() === normalizedName && number.toLowerCase() === normalizedNumber)   
-        })
-        return Boolean(result)
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      return { contacts: [newContact, ...contacts], name: '', number: '' };
+    });
+  };
+
+  deleteContact = id => {
+    this.setState(({ contacts }) => {
+      const newContacts = contacts.filter(contact => contact.id !== id);
+      return { contacts: newContacts };
+    });
+  };
+
+  isDublicate(name, number) {
+    const normalizedName = name.toLowerCase();
+    const normalizedNumber = number.toLowerCase();
+    const { contacts } = this.state;
+    const result = contacts.find(({ name, number }) => {
+      return (
+        name.toLowerCase() === normalizedName &&
+        number.toLowerCase() === normalizedNumber
+      );
+    });
+    return Boolean(result);
   }
 
   getFilteredContacts() {
-        const { filter, contacts } = this.state;
-        if (!filter) {   // cheking if filter input is empty (false) then do nothing
-            return contacts;
-        }
-        const normalizedFilter = filter.toLowerCase();
-        const result = contacts.filter(({ name, number }) => {
-            return (name.toLowerCase().includes(normalizedFilter) || number.toLowerCase().includes(normalizedFilter) )
-        })
-        return result;
+    const { filter, contacts } = this.state;
+    if (!filter) {
+      // cheking if filter input is empty (false) then do nothing
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
+    const result = contacts.filter(({ name, number }) => {
+      return (
+        name.toLowerCase().includes(normalizedFilter) ||
+        number.toLowerCase().includes(normalizedFilter)
+      );
+    });
+    return result;
   }
 
   handleFilter = ({ target }) => {
-    this.setState({ filter: target.value})
-  }
+    this.setState({ filter: target.value });
+  };
 
   render() {
     const { addContact, deleteContact, handleFilter } = this;
     const contacts = this.getFilteredContacts();
-       
+
     return (
       <div
         style={{
@@ -73,7 +97,7 @@ isDublicate(name, number) {
           width: '70vh',
           height: '100vh',
           display: 'flex',
-          flexDirection: "column",
+          flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 26,
@@ -85,8 +109,8 @@ isDublicate(name, number) {
 
         <h2>Contacts</h2>
         <Filter handleChange={handleFilter} />
-        <ContactList contacts={contacts} deleteContact={deleteContact} />        
+        <ContactList contacts={contacts} deleteContact={deleteContact} />
       </div>
     );
-  };
-};
+  }
+}
